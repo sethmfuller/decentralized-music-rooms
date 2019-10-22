40cd63543f5d143e69456f428453099bf52df983
Seth Fuller <semfulle@iupui.edu>
2019-10-21 20:43:29 -0400

    More ui work

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import Room from './views/Room.vue'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home
+    },
+    {
+      path: '/room',
+      name: 'room',
+      component: Room
     },
   ]
 })
---
+++ src/views/Room.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="room" class="page">
+    <div class="page_title">
+      <img src="../assets/icons/music_note.svg" alt="music note">
+      <h1>Seth's Electronic Music</h1>
+      <div class="more info">
+        
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'room'
+}
+</script>
