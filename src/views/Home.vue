788c7c7c91e7edcb2e828579d159bd09e556bccd
Umesh Raja <umeshraja0510@gmail.com>
2019-11-27 22:23:04 -0500

    ipfs added

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,9 +6,10 @@
     </div>
 
     <div class="rooms">
-      <RoomTile :addRoom=false @click="this.goToRoom()"></RoomTile>
+      <RoomTile :addRoom=false @click="goToRoom()"></RoomTile>
       <RoomTile :addRoom=false></RoomTile>
-      <RoomTile :addRoom=true></RoomTile>
+      <RoomTile :addRoom=true @click="this.createRoom()"></RoomTile>
+      <button v-on:click="createRoom('room'+Math.random())">Create room</button>
     </div>
 
     <div class="other_rooms">
@@ -39,6 +40,35 @@
 </template>
 
 <script>
+const IPFS = require('ipfs');
+const Room = require('ipfs-pubsub-room');
+// const fs = require('fs');
+const ipfs = new IPFS({
+  repo: repo(),
+  EXPERIMENTAL: {
+    pubsub: true
+  },
+  config: {
+    Addresses: {
+      Swarm: [
+        '/dns4/ws-star.discovery.libp2p.io/tcp/443/wss/p2p-websocket-star'
+      ]
+    }
+  }
+})
+
+ipfs.once('ready', () => ipfs.id((err, info) => {
+  if (err) { throw err }
+  console.log('IPFS node ready with address ' + info.id)
+}));
+  
+
+
+function repo () {
+  return 'ipfs/pubsub-demo/' + Math.random()
+}
+
+
 import RoomTile from '../components/RoomTile'
 export default {
   name: 'home',
@@ -51,6 +81,29 @@ export default {
     goToRoom() {
       this.$router.push('/room');
     },
+    createRoom(roomName) {
+      const room = Room(ipfs, roomName)
+//       fs.appendFile('roomNames.txt', roomName, function (err) {
+//   if (err) throw err;
+//   console.log('Saved!');
+// });
+
+  room.on('peer joined', (peer) => console.log('peer ' + peer + ' joined'))
+  room.on('peer left', (peer) => console.log('peer ' + peer + ' left'))
+
+  // send and receive messages
+
+  room.on('peer joined', (peer) => room.sendTo(peer, 'Hello ' + peer + '!'))
+  room.on('message', (message) => console.log('got message from ' + message.from + ': ' + message.data.toString()))
+
+  // broadcast message every 2 seconds
+
+  setInterval(() => room.broadcast('hey everyone!'), 2000)
+//   setInterval(() => fs.readFile('roomNames.txt', (err, data) => {
+//   if (err) throw err;
+//   console.log(data);
+// }), 5000)
+    }
   }
 }
 </script>
