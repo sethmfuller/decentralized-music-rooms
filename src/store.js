8e906b8d4ba191333b46db0201432809061b5028
Seth Fuller <semfulle@iupui.edu>
2019-12-05 20:34:59 -0500

    Converted current implementation to Vue proper implimentation

--- src/store.js
+++ src/store.js
@@ -5,10 +5,17 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    ipfsInstance: null,
+    ipfsInstanceId: null, 
   },
   mutations: {
+    setIPFSInstance(state, instance) {
+      state.ipfsInstance = instance;
+    },
 
+    setIPFSNodeId(state, id) {
+      state.ipfsInstanceId = id;
+    }
   },
   actions: {
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,25 +6,21 @@
     </div>
 
     <div class="rooms">
-      <RoomTile :addRoom=false @click="goToRoom()"></RoomTile>
-      <RoomTile :addRoom=false></RoomTile>
-      <RoomTile :addRoom=true @click="this.createRoom()"></RoomTile>
-      <button v-on:click="createRoom('room'+Math.random())">Create room</button>
+      <div @click="goToRoom">
+        <RoomTile :addRoom=false></RoomTile>
+      </div>
+      <div @click="goToRoom">
+        <RoomTile :addRoom=false></RoomTile>
+      </div>
+      <div @click="createRoom">
+        <RoomTile :addRoom=true></RoomTile>
+      </div>
     </div>
 
     <div class="other_rooms">
       <h2 class="other_rooms_title">Other Rooms</h2>
       <h3 class="other_rooms_subtitle">Listen to music in other great rooms!</h3>
       <div class="other_rooms_list">
-   <!--   <div class="item" v-for="roomName of roomNames">
-          <img src="../assets/icons/box_unchecked.svg" alt="unchecked checkbox">
-          <span class="room_name">{{roomName.name}}</span>
-          <div class="big_button join_button">Ask to Join {{roomName.peerHash}}</div>
-          <div class="members">
-            <img src="../assets/icons/user.svg" alt="user count">
-            <span>459</span>
-          </div>
-        </div>-->
         <div class="item">
           <img src="../assets/icons/box_unchecked.svg" alt="unchecked checkbox">
           <span class="room_name">Somebody's Music Room</span>
@@ -45,89 +41,50 @@
         </div>
       </div>
     </div>
+
+    <CreateRoomDialog v-if="createRoomDialogOpen" @close="closeDialog"></CreateRoomDialog>
+
   </div>
 </template>
 
 <script>
-const IPFS = require('ipfs');
-const Room = require('ipfs-pubsub-room');
-const fs = require('browserify-fs');
-// const readline = require('readline');
-// const readInterface = readline.createInterface({
-//     input: fs.createReadStream('./roomNamesGlobal.txt'),
-//     output: process.stdout,
-//     console: false
-// });
-const ipfs = new IPFS({
-  repo: repo(),
-  EXPERIMENTAL: {
-    pubsub: true
-  },
-  config: {
-    Addresses: {
-      Swarm: [
-        '/dns4/ws-star.discovery.libp2p.io/tcp/443/wss/p2p-websocket-star'
-      ]
-    }
-  }
-})
-
-var nodeId;
-var roomNames = [];
-class RoomName{
-  RoomName(name, peerHash){
-    this.name = name;
-    this.peerHash = peerHash;
-  }
-}
-ipfs.once('ready', () => ipfs.id((err, info) => {
-  if (err) { throw err }
-  console.log('IPFS node ready with address ' + info.id)
-  nodeId = info.id;
-}));
-  
-
-
-function repo () {
-  return 'ipfs/pubsub-demo/' + Math.random()
-}
-
-
 import RoomTile from '../components/RoomTile'
+import CreateRoomDialog from '../components/CreateRoomDialog'
+import { mapState } from 'vuex'
 export default {
   name: 'home',
 
   components: {
-    RoomTile
+    RoomTile,
+    CreateRoomDialog,
+  },
+
+  data() {
+    return {
+      createRoomDialogOpen: false,
+    }
+  },
+
+  computed: {
+    localComputed() {
+      return {}
+    },
+
+    ...mapState(['ipfsInstance'])
   },
 
   methods: {
     goToRoom() {
       this.$router.push('/room');
     },
-    createRoom(roomName) {
-      const room = Room(ipfs, roomName);
-      fs.appendFile('./roomNamesGlobal2.txt', roomName+' '+nodeId+'\n', function(err) {
-      if(err)
-      console.log(err);
-      });
 
-  room.on('peer joined', (peer) => console.log('peer ' + peer + ' joined'))
-  room.on('peer left', (peer) => console.log('peer ' + peer + ' left'))
-
-  // send and receive messages
-
-  room.on('peer joined', (peer) => room.sendTo(peer, 'Hello ' + peer + '!'))
-  room.on('message', (message) => console.log('got message from ' + message.from + ': ' + message.data.toString()))
-
-  // broadcast message every 2 seconds
-
-  setInterval(() => room.broadcast('hey everyone!'), 2000)
-  setInterval(() => fs.readFile('./roomNamesGlobal2.txt', 'utf-8', function(err, data) {
-            console.log(data);
-        }), 5000)
+    createRoom() {
+      this.createRoomDialogOpen = true;
     },
 
+    closeDialog() {
+      this.createRoomDialogOpen = false;
+    }
   }
 }
 </script>
